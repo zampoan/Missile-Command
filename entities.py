ddf5f07dce0a3ca418c861ba88725a524e403e3b
Zheng Yao Siah <49183569+zampoan@users.noreply.github.com>
2024-04-01 21:39:25 +1100

    added missile sprites

--- entities.py
+++ entities.py
@@ -1,11 +1,71 @@
 import pygame
+import random
+
+RESIZE_FACTOR = 0.7
 
 class City(pygame.sprite.Sprite):
-    def __init__(self, imgPath):
+    def __init__(self, imgPath, x, y):
         super().__init__()
 
-        self.img = pygame.image.load(imgPath)
-        self.rect = self.img.get_rect()
+        image = pygame.image.load(imgPath)
 
+        # Get the image width and height
+        width = image.get_width()
+        height = image.get_height()
+
+        self.image = pygame.transform.scale(image, (int(width * RESIZE_FACTOR), int(height * RESIZE_FACTOR)))
+
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+
+    def update(self):
+        pass
+
+
+class EnemyMissile(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+
+        spriteList = ['missile-command-assets/2D/missile_small.png',
+                      'missile-command-assets/2D/missile_medium.png',
+                      'missile-command-assets/2D/missile_large.png']
+
+        # select random choice 
+        image = pygame.image.load(random.choice(spriteList))
+
+        # Get the image width and height
+        width = image.get_width()
+        height = image.get_height()
+
+        self.image = pygame.transform.scale(image, (int(width * RESIZE_FACTOR), int(height * RESIZE_FACTOR)))
+
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+
+    def update(self):
+        pass
+
+class PlayerMissile(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+
+        # select random choice 
+        image = pygame.image.load('missile-command-assets/2D/anti_missile.png')
+
+        # Get the image width and height
+        width = image.get_width()
+        height = image.get_height()
+
+        self.image = pygame.transform.scale(image, (int(width * RESIZE_FACTOR), int(height * RESIZE_FACTOR)))
+
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+
+    def update(self):
+        self.rect.y -= 1
+        # pass
+
+
+        
 
 
--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import pygame
-import entities
+from entities import *
 
 SCREEN_WIDTH, SCREEN_HEIGHT = 1100, 900
-
-backgroundImage = pygame.image.load('missile-command-assets/2D/bg.png') # Img size is 1200 x 900
-backgroundImageRect = backgroundImage.get_rect()
 
 def backgroundMovement(screen, cursorX):
     """
     Make a moving background according to mouse position
     """
+
+    backgroundImage = pygame.image.load('missile-command-assets/2D/bg.png') # Img size is 1200 x 900
+    backgroundWater = pygame.image.load("missile-command-assets/2D/water.png")
+    backgroundCrosshair = pygame.image.load("missile-command-assets/2D/bg_resequ_crosshair.png")
+
      # Calculate background position
     backgroundX = 0 
     backgroundY = 0
@@ -33,12 +35,52 @@ def backgroundMovement(screen, cursorX):
     
     
     screen.blit(backgroundImage, (backgroundX, backgroundY))
+    screen.blit(backgroundCrosshair, (-50, 0))
+    screen.blit(backgroundWater, (-50, SCREEN_HEIGHT - 50))
+
+def spriteActivity():
+    cityImgPath = [
+        'missile-command-assets/2D/city01.png',
+        'missile-command-assets/2D/city02.png',
+        'missile-command-assets/2D/city03.png',
+        'missile-command-assets/2D/city04.png',
+        'missile-command-assets/2D/city05.png',
+        'missile-command-assets/2D/bridge.png'
+        ]
+
+    citySprites = pygame.sprite.Group()
+    enemyMissileSprites = pygame.sprite.Group()
+    playerMissileSprites = pygame.sprite.Group()
+
+    # Sprite activity
+    # Cities
+    city1 = City(cityImgPath[0], 20, SCREEN_HEIGHT - 105)
+    city2 = City(cityImgPath[1], 275, SCREEN_HEIGHT - 125)
+    city3 = City(cityImgPath[2], 450, SCREEN_HEIGHT - 100)
+    city4 = City(cityImgPath[3], 630, SCREEN_HEIGHT - 120)
+    city5 = City(cityImgPath[4], 900, SCREEN_HEIGHT - 100)
+    bridge1 = City(cityImgPath[5], 110, SCREEN_HEIGHT - 100)
+    citySprites.add(bridge1, city1, city2, city4, city3, city5, )
+
+    # Player Missiles
+    pMissile1 = PlayerMissile(90, SCREEN_HEIGHT - 55)
+    pMissile2 = PlayerMissile(345, SCREEN_HEIGHT - 75)
+    pMissile3 = PlayerMissile(520, SCREEN_HEIGHT - 50)
+    pMissile4 = PlayerMissile(700, SCREEN_HEIGHT - 70)
+    pMissile5 = PlayerMissile(970, SCREEN_HEIGHT - 50)
+    playerMissileSprites.add(pMissile1, pMissile2, pMissile3, pMissile4, pMissile5)
+
+    return citySprites, enemyMissileSprites, playerMissileSprites
 
 
 def main(screen):
-    clock = pygame.time.Clock()
 
+    clock = pygame.time.Clock()
     pygame.display.set_caption('Missile Command')
+    pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_CROSSHAIR)
+
+    # All sprites stores here
+    citySprites, enemyMissileSprites, playerMissileSprites = spriteActivity()
 
     running = True
 
@@ -51,15 +93,24 @@ def main(screen):
 
         # Get mouse position on screen for each frame
         cursorX, cursorY = pygame.mouse.get_pos()
+        print(cursorX, cursorY)
 
-        # screen.blit(backgroundImage, (backgroundX, backgroundY))
+        # Responsible for moving background
         backgroundMovement(screen, cursorX)
 
-        pygame.display.update()
+        # Update sprites
+        citySprites.update()
+        enemyMissileSprites.update()
+        playerMissileSprites.update()
+
+        # Draw sprites
+        citySprites.draw(screen)
+        enemyMissileSprites.draw(screen)
+        playerMissileSprites.draw(screen)
 
         pygame.display.flip()
 
-        clock.tick(60)
+        clock.tick(30)
 
     pygame.quit()
 
